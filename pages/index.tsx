7cdda1499522598ccab9bec63f5899fe899c8d18
ManuelDev <manueldeveloper17@gmail.com>
2023-02-10 14:24:35 -0500

    feat: add styles and framer motion to header

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,8 @@
 import Head from "next/head";
 
+// Components
+import Header from "../components/Header";
+
 export default function Home() {
   return (
     <>
@@ -7,7 +10,20 @@ export default function Home() {
         <title>Manuel`s Portfolio</title>
       </Head>
 
-      <h1 className="text-blue-800">Lets build and AWESOME portfolio</h1>
+      {/* Header */}
+      <Header />
+
+      {/* Hero */}
+
+      {/* About */}
+
+      {/* Experience */}
+
+      {/* Skills */}
+
+      {/* Projects */}
+
+      {/* Contact Me */}
     </>
   );
 }
