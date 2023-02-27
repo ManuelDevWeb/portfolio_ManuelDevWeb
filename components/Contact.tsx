72c8c4864a63b632669a452a57d5c94b9dadfab5
ManuelDev <manueldeveloper17@gmail.com>
2023-02-27 00:37:13 -0500

    feat: add useForm to Contact Component

--- components/Contact.tsx
+++ components/Contact.tsx
@@ -1,8 +1,26 @@
+import { useForm, SubmitHandler } from "react-hook-form";
 import { PhoneIcon, MapPinIcon, EnvelopeIcon } from "@heroicons/react/24/solid";
+
+type Inputs = {
+  name: string;
+  email: string;
+  subject: string;
+  message: string;
+};
 
 type Props = {};
 
 const Contact = ({}: Props) => {
+  // Register save the data from the form in the state, handleSubmit send the data to the onSubmit function
+  const { register, handleSubmit } = useForm<Inputs>();
+
+  // onSubmit is a function that receive the data from the form and do something when the form is submitted
+  const onSubmit: SubmitHandler<Inputs> = (formData) => {
+    // Open the default email client with the data from the form to send the email
+    window.location.href = `mailto:manuel.valencia@gmail?subject=${formData.subject}&body=Hi, my name is ${formData.name}. ${formData.message} (
+      ${formData.email})`;
+  };
+
   return (
     <div className="h-screen flex relative flex-col justify-evenly items-center text-center md:text-left md:flex-row max-w-7xl px-10 mx-auto">
       <h3 className="absolute top-24 uppercase tracking-[20px] text-gray-500 text-2xl">
@@ -31,6 +49,46 @@ const Contact = ({}: Props) => {
             <p className="text-2xl">Medellin/Colombia</p>
           </div>
         </div>
+
+        <form
+          onSubmit={handleSubmit(onSubmit)}
+          className="flex flex-col space-y-2 w-fit mx-auto"
+        >
+          <div className="flex space-x-2">
+            <input
+              // Register the input value in the state
+              {...register("name")}
+              placeholder="Name"
+              className="contactInput"
+              type="text"
+            />
+            <input
+              {...register("email")}
+              placeholder="Email"
+              className="contactInput"
+              type="email"
+            />
+          </div>
+
+          <input
+            {...register("subject")}
+            placeholder="Subject"
+            className="contactInput"
+            type="text"
+          />
+
+          <textarea
+            {...register("message")}
+            placeholder="Message"
+            className="contactInput"
+          />
+          <button
+            type="submit"
+            className="bg-[#F7AB0A] py-5 px-10 rounded-md text-black font-bold text-lg"
+          >
+            Submit
+          </button>
+        </form>
       </div>
     </div>
   );
--- pages/index.tsx
+++ pages/index.tsx
@@ -11,7 +11,7 @@ import Contact from "@/components/Contact";
 
 export default function Home() {
   return (
-    <div className="bg-[rgb(36,36,36)] text-white h-screen snap-y snap-mandatory overflow-scroll z-0 scroll-smooth">
+    <div className="bg-[rgb(36,36,36)] text-white h-screen snap-y snap-mandatory overflow-y-scroll overflow-x-auto z-0 scroll-smooth">
       <Head>
         <title>Manuel`s Portfolio</title>
       </Head>
